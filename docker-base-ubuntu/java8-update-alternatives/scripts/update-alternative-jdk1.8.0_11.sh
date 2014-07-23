2010a8256b7bdcbd70a9048b686be79d500e2471
Dirk Franssen <dirk.franssen@gmail.com>
2014-07-23 18:10:05 +0200

    restructured docker hierarchy

---
+++ docker-base-ubuntu/java8-update-alternatives/scripts/update-alternative-jdk1.8.0_11.sh
@@ -0,0 +1,53 @@
+update-alternatives --install /usr/bin/appletviewer appletviewer $JAVA_HOME/bin/appletviewer 100 --slave /usr/share/man/man1/appletviewer.1 appletviewer.1 $JAVA_HOME/man/man1/appletviewer.1
+update-alternatives --install /usr/bin/extcheck extcheck $JAVA_HOME/bin/extcheck 100 --slave /usr/share/man/man1/extcheck.1 extcheck.1 $JAVA_HOME/man/man1/extcheck.1
+update-alternatives --install /usr/bin/idlj idlj $JAVA_HOME/bin/idlj 100 --slave /usr/share/man/man1/idlj.1 idlj.1 $JAVA_HOME/man/man1/idlj.1
+update-alternatives --install /usr/bin/jar jar $JAVA_HOME/bin/jar 100 --slave /usr/share/man/man1/jar.1 jar.1 $JAVA_HOME/man/man1/jar.1
+update-alternatives --install /usr/bin/jarsigner jarsigner $JAVA_HOME/bin/jarsigner 100 --slave /usr/share/man/man1/jarsigner.1 jarsigner.1 $JAVA_HOME/man/man1/jarsigner.1
+update-alternatives --install /usr/bin/javac javac $JAVA_HOME/bin/javac 100 --slave /usr/share/man/man1/javac.1 javac.1 $JAVA_HOME/man/man1/javac.1
+update-alternatives --install /usr/bin/javadoc javadoc $JAVA_HOME/bin/javadoc 100 --slave /usr/share/man/man1/javadoc.1 javadoc.1 $JAVA_HOME/man/man1/javadoc.1
+update-alternatives --install /usr/bin/javafxpackager javafxpackager $JAVA_HOME/bin/javafxpackager 100 --slave /usr/share/man/man1/javafxpackager.1 javafxpackager.1 $JAVA_HOME/man/man1/javafxpackager.1
+update-alternatives --install /usr/bin/javah javah $JAVA_HOME/bin/javah 100 --slave /usr/share/man/man1/javah.1 javah.1 $JAVA_HOME/man/man1/javah.1
+update-alternatives --install /usr/bin/javap javap $JAVA_HOME/bin/javap 100 --slave /usr/share/man/man1/javap.1 javap.1 $JAVA_HOME/man/man1/javap.1
+update-alternatives --install /usr/bin/jcmd jcmd $JAVA_HOME/bin/jcmd 100 --slave /usr/share/man/man1/jcmd.1 jcmd.1 $JAVA_HOME/man/man1/jcmd.1
+update-alternatives --install /usr/bin/jconsole jconsole $JAVA_HOME/bin/jconsole 100 --slave /usr/share/man/man1/jconsole.1 jconsole.1 $JAVA_HOME/man/man1/jconsole.1
+update-alternatives --install /usr/bin/jdb jdb $JAVA_HOME/bin/jdb 100 --slave /usr/share/man/man1/jdb.1 jdb.1 $JAVA_HOME/man/man1/jdb.1
+update-alternatives --install /usr/bin/jdeps jdeps $JAVA_HOME/bin/jdeps 100 --slave /usr/share/man/man1/jdeps.1 jdeps.1 $JAVA_HOME/man/man1/jdeps.1
+update-alternatives --install /usr/bin/jhat jhat $JAVA_HOME/bin/jhat 100 --slave /usr/share/man/man1/jhat.1 jhat.1 $JAVA_HOME/man/man1/jhat.1
+update-alternatives --install /usr/bin/jinfo jinfo $JAVA_HOME/bin/jinfo 100 --slave /usr/share/man/man1/jinfo.1 jinfo.1 $JAVA_HOME/man/man1/jinfo.1
+update-alternatives --install /usr/bin/jmap jmap $JAVA_HOME/bin/jmap 100 --slave /usr/share/man/man1/jmap.1 jmap.1 $JAVA_HOME/man/man1/jmap.1
+update-alternatives --install /usr/bin/jmc jmc $JAVA_HOME/bin/jmc 100 --slave /usr/share/man/man1/jmc.1 jmc.1 $JAVA_HOME/man/man1/jmc.1
+update-alternatives --install /usr/bin/jps jps $JAVA_HOME/bin/jps 100 --slave /usr/share/man/man1/jps.1 jps.1 $JAVA_HOME/man/man1/jps.1
+update-alternatives --install /usr/bin/jrunscript jrunscript $JAVA_HOME/bin/jrunscript 100 --slave /usr/share/man/man1/jrunscript.1 jrunscript.1 $JAVA_HOME/man/man1/jrunscript.1
+update-alternatives --install /usr/bin/jsadebugd jsadebugd $JAVA_HOME/bin/jsadebugd 100 --slave /usr/share/man/man1/jsadebugd.1 jsadebugd.1 $JAVA_HOME/man/man1/jsadebugd.1
+update-alternatives --install /usr/bin/jstack jstack $JAVA_HOME/bin/jstack 100 --slave /usr/share/man/man1/jstack.1 jstack.1 $JAVA_HOME/man/man1/jstack.1
+update-alternatives --install /usr/bin/jstat jstat $JAVA_HOME/bin/jstat 100 --slave /usr/share/man/man1/jstat.1 jstat.1 $JAVA_HOME/man/man1/jstat.1
+update-alternatives --install /usr/bin/jstatd jstatd $JAVA_HOME/bin/jstatd 100 --slave /usr/share/man/man1/jstatd.1 jstatd.1 $JAVA_HOME/man/man1/jstatd.1
+update-alternatives --install /usr/bin/jvisualvm jvisualvm $JAVA_HOME/bin/jvisualvm 100 --slave /usr/share/man/man1/jvisualvm.1 jvisualvm.1 $JAVA_HOME/man/man1/jvisualvm.1
+update-alternatives --install /usr/bin/native2ascii native2ascii $JAVA_HOME/bin/native2ascii 100 --slave /usr/share/man/man1/native2ascii.1 native2ascii.1 $JAVA_HOME/man/man1/native2ascii.1
+update-alternatives --install /usr/bin/rmic rmic $JAVA_HOME/bin/rmic 100 --slave /usr/share/man/man1/rmic.1 rmic.1 $JAVA_HOME/man/man1/rmic.1
+update-alternatives --install /usr/bin/schemagen schemagen $JAVA_HOME/bin/schemagen 100 --slave /usr/share/man/man1/schemagen.1 schemagen.1 $JAVA_HOME/man/man1/schemagen.1
+update-alternatives --install /usr/bin/serialver serialver $JAVA_HOME/bin/serialver 100 --slave /usr/share/man/man1/serialver.1 serialver.1 $JAVA_HOME/man/man1/serialver.1
+update-alternatives --install /usr/bin/wsgen wsgen $JAVA_HOME/bin/wsgen 100 --slave /usr/share/man/man1/wsgen.1 wsgen.1 $JAVA_HOME/man/man1/wsgen.1
+update-alternatives --install /usr/bin/wsimport wsimport $JAVA_HOME/bin/wsimport 100 --slave /usr/share/man/man1/wsimport.1 wsimport.1 $JAVA_HOME/man/man1/wsimport.1
+update-alternatives --install /usr/bin/xjc xjc $JAVA_HOME/bin/xjc 100 --slave /usr/share/man/man1/xjc.1 xjc.1 $JAVA_HOME/man/man1/xjc.1
+
+update-alternatives --install /usr/bin/ControlPanel ControlPanel $JAVA_HOME/jre/bin/ControlPanel 100
+update-alternatives --install /usr/bin/java java $JAVA_HOME/jre/bin/java 100 --slave /usr/share/man/man1/java.1 java.1 $JAVA_HOME/man/man1/java.1
+update-alternatives --install /usr/bin/javaws javaws $JAVA_HOME/jre/bin/javaws 100 --slave /usr/share/man/man1/javaws.1 javaws.1 $JAVA_HOME/man/man1/javaws.1
+update-alternatives --install /usr/bin/jcontrol jcontrol $JAVA_HOME/jre/bin/jcontrol 100
+update-alternatives --install /usr/bin/jjs jjs $JAVA_HOME/jre/bin/jjs 100 --slave /usr/share/man/man1/jjs.1 jjs.1 $JAVA_HOME/man/man1/jjs.1
+update-alternatives --install /usr/bin/keytool keytool $JAVA_HOME/jre/bin/keytool 100 --slave /usr/share/man/man1/keytool.1 keytool.1 $JAVA_HOME/man/man1/keytool.1
+update-alternatives --install /usr/bin/orbd orbd $JAVA_HOME/jre/bin/orbd 100 --slave /usr/share/man/man1/orbd.1 orbd.1 $JAVA_HOME/man/man1/orbd.1
+update-alternatives --install /usr/bin/pack200 pack200 $JAVA_HOME/jre/bin/pack200 100 --slave /usr/share/man/man1/pack200.1 pack200.1 $JAVA_HOME/man/man1/pack200.1
+update-alternatives --install /usr/bin/policytool policytool $JAVA_HOME/jre/bin/policytool 100 --slave /usr/share/man/man1/policytool.1 policytool.1 $JAVA_HOME/man/man1/policytool.1
+update-alternatives --install /usr/bin/rmid rmid $JAVA_HOME/jre/bin/rmid 100 --slave /usr/share/man/man1/rmid.1 rmid.1 $JAVA_HOME/man/man1/rmid.1
+update-alternatives --install /usr/bin/rmiregistry rmiregistry $JAVA_HOME/jre/bin/rmiregistry 100 --slave /usr/share/man/man1/rmiregistry.1 rmiregistry.1 $JAVA_HOME/man/man1/rmiregistry.1
+update-alternatives --install /usr/bin/servertool servertool $JAVA_HOME/jre/bin/servertool 100 --slave /usr/share/man/man1/servertool.1 servertool.1 $JAVA_HOME/man/man1/servertool.1
+update-alternatives --install /usr/bin/tnameserv tnameserv $JAVA_HOME/jre/bin/tnameserv 100 --slave /usr/share/man/man1/tnameserv.1 tnameserv.1 $JAVA_HOME/man/man1/tnameserv.1
+update-alternatives --install /usr/bin/unpack200 unpack200 $JAVA_HOME/jre/bin/unpack200 100 --slave /usr/share/man/man1/unpack200.1 unpack200.1 $JAVA_HOME/man/man1/unpack200.1
+
+#thos plugins are currently not available/needed in docker base image
+#update-alternatives --install /usr/lib/xulrunner-addons/plugins/libjavaplugin.so xulrunner-1.9-javaplugin.so $JAVA_HOME/jre/lib/amd64/libnpjp2.so 100
+#update-alternatives --install /usr/lib/mozilla/plugins/libjavaplugin.so mozilla-javaplugin.so $JAVA_HOME/jre/lib/amd64/libnpjp2.so 100
+
+update-java-alternatives -s $JAVA_ALIAS
